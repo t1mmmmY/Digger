2737de93efeca81234883c927b338f651229a1c0
unknown <dovganichd@radchenkov.digicode.ua>
2015-07-16 17:15:04 +0300

    Purchases

---
+++ Assets/Scripts/Store/AndroidStore.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Soomla.Store;
+
+public class AndroidStore : IStoreAssets 
+{
+
+    public const string CHARACTER_CURRENCY_ITEM_ID = "currency_character";
+    public const string CHARACTER_CURRENCY_PRODUCT_ID = "android.digger.testcharacter";
+    public const string CHARACTER_ITEM_ID = "selected_character";
+
+    public static VirtualCurrency CHARACTER_CURRENCY = new VirtualCurrency
+        (
+                "Character",										// name
+                "Buy selected character",												// description
+                CHARACTER_CURRENCY_ITEM_ID							// item id
+        );
+
+    
+
+    public static VirtualGood SELECTED_CHARACTER = new SingleUseVG(
+                "Selected character",                                       		// name
+                "Character for real money", // description
+                "Character",                                       		// item id
+                new PurchaseWithVirtualItem(CHARACTER_CURRENCY_ITEM_ID, 1)); // the way this virtual good is purchased
+
+    public static VirtualCurrencyPack CHARACTER_PACK = new VirtualCurrencyPack(
+                "10 Muffins",                                   // name
+                "Test refund of an item",                       // description
+                "muffins_10",                                   // item id
+                10,												// number of currencies in the pack
+                CHARACTER_CURRENCY_ITEM_ID,                        // the currency associated with this pack
+                new PurchaseWithMarket(CHARACTER_CURRENCY_PRODUCT_ID, 0.99)
+        );
+
+    public static VirtualCategory GENERAL_CATEGORY = new VirtualCategory(
+                "General", new List<string>(new string[] { CHARACTER_ITEM_ID })
+        );
+
+
+    public int GetVersion()
+    {
+        return 0;
+        //throw new System.NotImplementedException();
+    }
+
+    public VirtualCurrency[] GetCurrencies()
+    {
+        return new VirtualCurrency[] { CHARACTER_CURRENCY };
+        //throw new System.NotImplementedException();
+    }
+
+    public VirtualGood[] GetGoods()
+    {
+        return new VirtualGood[] { SELECTED_CHARACTER };
+        //throw new System.NotImplementedException();
+    }
+
+    public VirtualCurrencyPack[] GetCurrencyPacks()
+    {
+        return new VirtualCurrencyPack[] { CHARACTER_PACK };
+        //throw new System.NotImplementedException();
+    }
+
+    public VirtualCategory[] GetCategories()
+    {
+        return new VirtualCategory[] { GENERAL_CATEGORY };
+        //throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/Store/InGameStore.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Soomla.Store;
+
+public class InGameStore : MonoBehaviour 
+{
+
+	void Start ()
+    {
+        StoreEvents.OnSoomlaStoreInitialized += OnSoomlaStoreInitialized;
+        SoomlaStore.Initialize(new AndroidStore());
+        StoreEvents.OnItemPurchaseStarted += OnItemPurchaseStarted;
+        StoreEvents.OnItemPurchased += OnItemPurchased;
+        StoreEvents.OnMarketPurchaseStarted += OnMarketPurchaseStarted;
+        StoreEvents.OnMarketPurchase += OnMarketPurchase;
+        StoreEvents.OnMarketPurchaseCancelled += OnMarketPurchaseCancelled;
+	}
+
+    void OnDestroy()
+    {
+        StoreEvents.OnSoomlaStoreInitialized -= OnSoomlaStoreInitialized;
+        StoreEvents.OnItemPurchaseStarted -= OnItemPurchaseStarted;
+        StoreEvents.OnItemPurchased -= OnItemPurchased;
+        StoreEvents.OnMarketPurchaseStarted -= OnMarketPurchaseStarted;
+        StoreEvents.OnMarketPurchase -= OnMarketPurchase;
+        StoreEvents.OnMarketPurchaseCancelled -= OnMarketPurchaseCancelled;
+    }
+
+    public void OnSoomlaStoreInitialized()
+    {
+        Debug.Log("Store initialized");
+        // ... your game specific implementation here ...
+    }
+
+    void OnItemPurchaseStarted(PurchasableVirtualItem item)
+    {
+
+    }
+
+    void OnItemPurchased(PurchasableVirtualItem item, string name)
+    {
+
+    }
+
+    void OnMarketPurchaseStarted(PurchasableVirtualItem item)
+    {
+        // pvi - the PurchasableVirtualItem whose purchase operation has just started
+
+        // ... your game specific implementation here ...
+    }
+
+    public void OnMarketPurchase(PurchasableVirtualItem pvi, string payload, Dictionary<string, string> extra)
+    {
+        // pvi - the PurchasableVirtualItem that was just purchased
+        // payload - a text that you can give when you initiate the purchase operation and
+        //    you want to receive back upon completion
+        // extra - contains platform specific information about the market purchase
+        //    Android: The "extra" dictionary will contain: 'token', 'orderId', 'originalJson', 'signature', 'userId'
+        //    iOS: The "extra" dictionary will contain: 'receiptUrl', 'transactionIdentifier', 'receiptBase64', 'transactionDate', 'originalTransactionDate', 'originalTransactionIdentifier'
+
+        // ... your game specific implementation here ...
+    }
+
+    public void OnMarketPurchaseCancelled(PurchasableVirtualItem pvi)
+    {
+        // pvi - the PurchasableVirtualItem whose purchase operation was cancelled
+
+        // ... your game specific implementation here ...
+    }
+	
+}
