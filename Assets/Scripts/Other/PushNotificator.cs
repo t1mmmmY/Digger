bfc07036e6810814f1e9d9ea0fd0e1abbb0c6d2e
unknown <dovganichd@radchenkov.digicode.ua>
2015-07-20 18:06:52 +0300

    Pushwoosh

---
+++ Assets/Scripts/Other/PushNotificator.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PushNotificator : MonoBehaviour
+{
+
+    string notificationText = "Pushwoosh is not initialized";
+
+    // Use this for initialization
+    void Start()
+    {
+        Pushwoosh.Instance.OnRegisteredForPushNotifications += onRegisteredForPushNotifications;
+        Pushwoosh.Instance.OnFailedToRegisteredForPushNotifications += onFailedToRegisteredForPushNotifications;
+        Pushwoosh.Instance.OnPushNotificationsReceived += onPushNotificationsReceived;
+    }
+
+    void onRegisteredForPushNotifications(string token)
+    {
+        notificationText = "Received token: \n" + token;
+
+        //do handling here
+        Debug.Log(notificationText);
+    }
+
+    void onFailedToRegisteredForPushNotifications(string error)
+    {
+        notificationText = "Error ocurred while registering to push notifications: \n" + error;
+
+        //do handling here
+        Debug.Log(notificationText);
+    }
+
+    void onPushNotificationsReceived(string payload)
+    {
+        notificationText = "Received push notificaiton: \n" + payload;
+
+        //do handling here
+        Debug.Log(notificationText);
+    }
+}
