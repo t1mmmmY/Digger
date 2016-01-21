8dc6044b05e8ba14eb92cb323de2f832e1a52efe
denis <dovganichd@digicode.net>
2016-01-21 23:47:17 +0100

    Login working!!!

--- Assets/Scripts/Multiplayer/MultiplayerController.cs
+++ Assets/Scripts/Multiplayer/MultiplayerController.cs
@@ -1,15 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 using GooglePlayGames;
-using GooglePlayGames.BasicApi.Multiplayer;
-using Newtonsoft.Json;
-using VoxelBusters.NativePlugins;
-
-using GooglePlayGames.BasicApi;
 using UnityEngine.SocialPlatforms;
 
-public class MultiplayerController : RealTimeMultiplayerListener
+public class MultiplayerController : MonoBehaviour
 {
 	private static MultiplayerController _instance = null;
 	public static System.Action<bool, string, byte[]> onRealTimeMessageReceived;
@@ -24,24 +18,33 @@ public class MultiplayerController : RealTimeMultiplayerListener
 #endif
 
 
-	private MultiplayerController() 
+	public void Awake() 
 	{
+		_instance = this;
+
 #if UNITY_ANDROID
-		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
-			// enables saving game progress.
-			.EnableSavedGames()
-				// registers a callback to handle game invitations received while the game is not running.
-//				.WithInvitationDelegate(() => {})
-				// registers a callback for turn based match notifications received while the
-				// game is not running.
-//				.WithMatchDelegate(() => {})
-				.Build();
-		
-		PlayGamesPlatform.InitializeInstance(config);
 		// recommended for debugging:
 		PlayGamesPlatform.DebugLogEnabled = true;
+		
 		// Activate the Google Play Games platform
-		PlayGamesPlatform.Activate();
+		PlayGamesPlatform.Activate ();
+
+
+//		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+//			// enables saving game progress.
+//			.EnableSavedGames()
+//				// registers a callback to handle game invitations received while the game is not running.
+////				.WithInvitationDelegate(() => {})
+//				// registers a callback for turn based match notifications received while the
+//				// game is not running.
+////				.WithMatchDelegate(() => {})
+//				.Build();
+//		
+//		PlayGamesPlatform.InitializeInstance(config);
+//		// recommended for debugging:
+//		PlayGamesPlatform.DebugLogEnabled = true;
+//		// Activate the Google Play Games platform
+//		PlayGamesPlatform.Activate();
 
 
 
@@ -55,10 +58,10 @@ public class MultiplayerController : RealTimeMultiplayerListener
 	{
 		get 
 		{
-			if (_instance == null) 
-			{
-				_instance = new MultiplayerController();
-			}
+//			if (_instance == null) 
+//			{
+//				_instance = new MultiplayerController();
+//			}
 			return _instance;
 		}
 	}
@@ -69,16 +72,15 @@ public class MultiplayerController : RealTimeMultiplayerListener
 #if UNITY_ANDROID
 		//ANDROID CHANGES
 //		UnityEngine.Social.localUser.Authenticate(callback);
-		
 
 
-		if (!PlayGamesPlatform.Instance.localUser.authenticated) 
-		{
-			PlayGamesPlatform.Instance.localUser.Authenticate((bool success) => 
+//		if (!PlayGamesPlatform.Instance.localUser.authenticated) 
+//		{
+			Social.localUser.Authenticate((bool success) => 
 			{
 				if (success) 
 				{
-					Debug.Log ("We're signed in! Welcome " + PlayGamesPlatform.Instance.localUser.userName);
+					Debug.Log ("We're signed in! Welcome!");
 					// We could start our game now
 				} 
 				else 
@@ -91,12 +93,12 @@ public class MultiplayerController : RealTimeMultiplayerListener
 					callback(success);
 				}
 			});
-		} 
-		else 
-		{
-			Debug.Log ("You're already signed in.");
+//		} 
+//		else 
+//		{
+//			Debug.Log ("You're already signed in.");
 //			// We could also start our game now
-		}
+//		}
 #elif UNITY_IOS
 
 		UnityEngine.Social.localUser.Authenticate(callback);
@@ -107,49 +109,69 @@ public class MultiplayerController : RealTimeMultiplayerListener
 	public void TrySilentSignIn(System.Action<bool> callback) 
 	{
 #if UNITY_ANDROID
-//		SignIn(callback);
-		
+		SignIn(callback);
 
 
-		if (!PlayGamesPlatform.Instance.localUser.authenticated) 
-		{
-			PlayGamesPlatform.Instance.Authenticate((bool success) => 
-			{
-				if (success) 
-				{
-					Debug.Log ("Silently signed in! Welcome " + PlayGamesPlatform.Instance.localUser.userName);
-				} 
-				else 
-				{
-					Debug.Log ("Oh... we're not signed in.");
-				}
 
-				if (callback != null)
-				{
-					callback(success);
-				}
-
-			}, true);
-		} 
-		else 
-		{
-			Debug.Log("We're already signed in");
-		}
+//		if (!PlayGamesPlatform.Instance.localUser.authenticated) 
+//		{
+//			PlayGamesPlatform.Instance.Authenticate((bool success) => 
+//			{
+//				if (success) 
+//				{
+//					Debug.Log ("Silently signed in! Welcome " + PlayGamesPlatform.Instance.localUser.userName);
+//				} 
+//				else 
+//				{
+//					Debug.Log ("Oh... we're not signed in.");
+//				}
+//
+//				if (callback != null)
+//				{
+//					callback(success);
+//				}
+//
+//			}, true);
+//		} 
+//		else 
+//		{
+//			Debug.Log("We're already signed in");
+//		}
 
 #elif UNITY_IOS
 		SignIn(callback);
 #endif
 	}
 
-	private void OnComleteAuthentication(bool result)
-	{
-		Debug.Log("OnComleteAuthentication " + result);
-	}
+//	private void OnComleteAuthentication(bool result)
+//	{
+//		Debug.Log("OnComleteAuthentication " + result);
+//	}
 
 	public void SetBestSore(int score, System.Action<bool> callback = null)
 	{
 #if UNITY_ANDROID
-		UnityEngine.Social.ReportScore((long)score, CONST.ANDROID_LEADERBOARD_ID, callback);
+		if (Social.localUser.authenticated) 
+		{
+			Social.ReportScore ((long)score, CONST.ANDROID_LEADERBOARD_ID, (bool success) =>
+			                    {
+				if (success) {
+//					Debug.Log ("Update Score Success");
+					
+				} else {
+//					Debug.Log ("Update Score Fail");
+				}
+
+				if (callback != null)
+				{
+					callback(success);
+				}
+			});
+		}
+
+//		Social.ReportScore((long)score, CONST.ANDROID_LEADERBOARD_ID, callback);
+		
+		//		UnityEngine.Social.ReportScore((long)score, CONST.ANDROID_LEADERBOARD_ID, callback);
 		
 
 //		PlayGamesPlatform.Instance.ReportScore((long)score, "CgkImYnr8fAKEAIQAg", callback);
@@ -158,203 +180,207 @@ public class MultiplayerController : RealTimeMultiplayerListener
 #endif
 	}
 
-	public int GetRang()
-	{
-		//Local for now
-		return PlayerPrefs.GetInt("PlayerRang", 100);
-	}
+//	public int GetRang()
+//	{
+//		//Local for now
+//		return PlayerPrefs.GetInt("PlayerRang", 100);
+//	}
 
 
-	public void ChangeRang(int score, System.Action<bool> callback = null)
-	{
-#if UNITY_ANDROID
-//		PlayGamesPlatform.Instance.ReportScore((long)(GetRang() + score), "CgkImYnr8fAKEAIQAw", callback);
-#elif UNITY_IOS
-#endif
-		PlayerPrefs.SetInt("PlayerRang", GetRang() + score);
-	}
+//	public void ChangeRang(int score, System.Action<bool> callback = null)
+//	{
+//#if UNITY_ANDROID
+////		PlayGamesPlatform.Instance.ReportScore((long)(GetRang() + score), "CgkImYnr8fAKEAIQAw", callback);
+//#elif UNITY_IOS
+//#endif
+//		PlayerPrefs.SetInt("PlayerRang", GetRang() + score);
+//	}
 
 	public void ShowLeaderboard()
 	{
 #if UNITY_ANDROID
-		PlayGamesPlatform.Instance.ShowLeaderboardUI();//"CgkImYnr8fAKEAIQAg");
+		//        Social.ShowLeaderboardUI (); // Show all leaderboard
+		((PlayGamesPlatform)Social.Active).ShowLeaderboardUI (CONST.ANDROID_LEADERBOARD_ID); // Show current (Active) leaderboard
+
+//		Social.ShowLeaderboardUI();
+//		PlayGamesPlatform.Instance.ShowLeaderboardUI();//"CgkImYnr8fAKEAIQAg");
 #elif UNITY_IOS
 		UnityEngine.SocialPlatforms.GameCenter.GameCenterPlatform.ShowLeaderboardUI(CONST.IOS_LEADERBOARD_ID, UnityEngine.SocialPlatforms.TimeScope.Today);
 #endif
 	}
 
 
-	public void StartMatchMakingRealTime() 
-	{
-#if UNITY_EDITOR || UNITY_WEBPLAYER
-#else
-		PlayGamesPlatform.Instance.RealTime.CreateWithInvitationScreen(minimumOpponents, maximumOpponents, gameVariation, this);
-#endif
-	}
+//	public void StartMatchMakingRealTime() 
+//	{
+//#if UNITY_EDITOR || UNITY_WEBPLAYER
+//#else
+//		PlayGamesPlatform.Instance.RealTime.CreateWithInvitationScreen(minimumOpponents, maximumOpponents, gameVariation, this);
+//#endif
+//	}
 
-	public void Disconnect()
-	{
-#if UNITY_EDITOR || UNITY_WEBPLAYER
-#else
-		PlayGamesPlatform.Instance.RealTime.LeaveRoom();
-#endif
-	}
-
-	public void StartMatchMakingRealTimeFast() 
-	{
-#if UNITY_EDITOR || UNITY_WEBPLAYER
-#else
-		PlayGamesPlatform.Instance.RealTime.CreateQuickGame(minimumOpponents, maximumOpponents, gameVariation, this);
-#endif
-	}
+//	public void Disconnect()
+//	{
+//#if UNITY_EDITOR || UNITY_WEBPLAYER
+//#else
+//		PlayGamesPlatform.Instance.RealTime.LeaveRoom();
+//#endif
+//	}
 //
-	private void TurnCallback(bool success, TurnBasedMatch match)
-	{
-	}
+//	public void StartMatchMakingRealTimeFast() 
+//	{
+//#if UNITY_EDITOR || UNITY_WEBPLAYER
+//#else
+//		PlayGamesPlatform.Instance.RealTime.CreateQuickGame(minimumOpponents, maximumOpponents, gameVariation, this);
+//#endif
+//	}
+//
+//	private void TurnCallback(bool success, TurnBasedMatch match)
+//	{
+//	}
 //	
 //	
-	private void ShowMPStatus(string message) 
-	{
-		Debug.Log(message);
-	}
+//	private void ShowMPStatus(string message) 
+//	{
+//		Debug.Log(message);
+//	}
+////
+////
+//	public void OnRoomSetupProgress (float percent)
+//	{
+//		ShowMPStatus ("We are " + percent + "% done with setup");
+//	}
+
+//	public void OnRoomConnected (bool success)
+//	{
+//	}
 //
+//	public void OnLeftRoom ()
+//	{
+//		ShowMPStatus ("We have left the room. We should probably perform some clean-up tasks.");
+//	}
 //
-	public void OnRoomSetupProgress (float percent)
-	{
-		ShowMPStatus ("We are " + percent + "% done with setup");
-	}
+//	public void OnPeersConnected(string[] participantIds)
+//	{
+//		foreach (string participantID in participantIds) 
+//		{
+//			ShowMPStatus ("Player " + participantID + " has joined.");
+//		}
+//	}
+//
+//	public void OnPeersDisconnected (string[] participantIds)
+//	{
+//		foreach (string participantID in participantIds) 
+//		{
+//			ShowMPStatus ("Player " + participantID + " has left.");
+//		}
+//	}
+//
+//	public void OnRealTimeMessageReceived (bool isReliable, string senderId, byte[] data)
+//	{
+//		if (onRealTimeMessageReceived != null)
+//		{
+//			onRealTimeMessageReceived(isReliable, senderId, data);
+//		}
+//		ShowMPStatus ("We have received some gameplay messages from participant ID:" + senderId);
+//	}
 
-	public void OnRoomConnected (bool success)
-	{
-	}
-
-	public void OnLeftRoom ()
-	{
-		ShowMPStatus ("We have left the room. We should probably perform some clean-up tasks.");
-	}
-
-	public void OnPeersConnected(string[] participantIds)
-	{
-		foreach (string participantID in participantIds) 
-		{
-			ShowMPStatus ("Player " + participantID + " has joined.");
-		}
-	}
-
-	public void OnPeersDisconnected (string[] participantIds)
-	{
-		foreach (string participantID in participantIds) 
-		{
-			ShowMPStatus ("Player " + participantID + " has left.");
-		}
-	}
-
-	public void OnRealTimeMessageReceived (bool isReliable, string senderId, byte[] data)
-	{
-		if (onRealTimeMessageReceived != null)
-		{
-			onRealTimeMessageReceived(isReliable, senderId, data);
-		}
-		ShowMPStatus ("We have received some gameplay messages from participant ID:" + senderId);
-	}
-
-	public static byte[] Serialize(object obj)
-	{
-		string text = JsonConvert.SerializeObject(obj);
-		return MultiplayerController.GetBytes(text);
-	}
-
-	public static object Deserialize(byte[] data, System.Type type = null)
-	{
-		string text = GetString(data);
-		if (type == null)
-		{
-			return JsonConvert.DeserializeObject(text);
-		}
-		else
-		{
-			object obj = JsonConvert.DeserializeObject(text, type);
-			return obj;
-		}
-	}
-
-	public static byte[] GetBytes(string str)
-	{
-		byte[] bytes = new byte[str.Length * sizeof(char)];
-		System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
-		return bytes;
-	}
+//	public static byte[] Serialize(object obj)
+//	{
+//		string text = JsonConvert.SerializeObject(obj);
+//		return MultiplayerController.GetBytes(text);
+//	}
+//
+//	public static object Deserialize(byte[] data, System.Type type = null)
+//	{
+//		string text = GetString(data);
+//		if (type == null)
+//		{
+//			return JsonConvert.DeserializeObject(text);
+//		}
+//		else
+//		{
+//			object obj = JsonConvert.DeserializeObject(text, type);
+//			return obj;
+//		}
+//	}
+//
+//	public static byte[] GetBytes(string str)
+//	{
+//		byte[] bytes = new byte[str.Length * sizeof(char)];
+//		System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+//		return bytes;
+//	}
+//	
+//	public static string GetString(byte[] bytes)
+//	{
+//		char[] chars = new char[bytes.Length / sizeof(char)];
+//		System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+//		return new string(chars);
+//	}
 	
-	public static string GetString(byte[] bytes)
-	{
-		char[] chars = new char[bytes.Length / sizeof(char)];
-		System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
-		return new string(chars);
-	}
-	
-	public void SendRealTimeMessage(object data, bool toAll = false, bool reliable = true)
-	{
-		byte[] bData = MultiplayerController.Serialize(data);
-#if UNITY_EDITOR || UNITY_WEBPLAYER
-		OnRealTimeMessageReceived(true, "", bData);
-#else
-		if (!toAll)
-		{
-			foreach (Participant opponent in PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants())
-			{
-				if (opponent != PlayGamesPlatform.Instance.RealTime.GetSelf())
-				{
-					PlayGamesPlatform.Instance.RealTime.SendMessage(reliable, opponent.ParticipantId, bData);
-				}
-				else
-				{
-				}
-			}
-		}
-		else
-		{
-			foreach (Participant opponent in PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants())
-			{
-				if (opponent != PlayGamesPlatform.Instance.RealTime.GetSelf())
-				{
-					Debug.LogWarning("Opponent");
-				}
-				else
-				{
-					Debug.LogWarning("Me");
-				}
-			}
-			PlayGamesPlatform.Instance.RealTime.SendMessageToAll(reliable, bData);
-		}
-#endif
-	}
-
-	public void OnParticipantLeft (Participant participant)
-	{
-//		throw new System.NotImplementedException ();
-	}
-
-	public bool IsFirstPlayer()
-	{
-#if UNITY_EDITOR || UNITY_WEBPLAYER
-		return true;
-#else
-
-		int number = 0;
-		foreach (Participant participant in PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants())
-		{
-			if (participant == PlayGamesPlatform.Instance.RealTime.GetSelf() && number == 0)
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-			number++;
-		}
-		return false;
-#endif
-	}
+//	public void SendRealTimeMessage(object data, bool toAll = false, bool reliable = true)
+//	{
+//		byte[] bData = MultiplayerController.Serialize(data);
+//#if UNITY_EDITOR || UNITY_WEBPLAYER
+//		OnRealTimeMessageReceived(true, "", bData);
+//#else
+//		if (!toAll)
+//		{
+//			foreach (Participant opponent in PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants())
+//			{
+//				if (opponent != PlayGamesPlatform.Instance.RealTime.GetSelf())
+//				{
+//					PlayGamesPlatform.Instance.RealTime.SendMessage(reliable, opponent.ParticipantId, bData);
+//				}
+//				else
+//				{
+//				}
+//			}
+//		}
+//		else
+//		{
+//			foreach (Participant opponent in PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants())
+//			{
+//				if (opponent != PlayGamesPlatform.Instance.RealTime.GetSelf())
+//				{
+//					Debug.LogWarning("Opponent");
+//				}
+//				else
+//				{
+//					Debug.LogWarning("Me");
+//				}
+//			}
+//			PlayGamesPlatform.Instance.RealTime.SendMessageToAll(reliable, bData);
+//		}
+//#endif
+//	}
+//
+//	public void OnParticipantLeft (Participant participant)
+//	{
+////		throw new System.NotImplementedException ();
+//	}
+//
+//	public bool IsFirstPlayer()
+//	{
+//#if UNITY_EDITOR || UNITY_WEBPLAYER
+//		return true;
+//#else
+//
+//		int number = 0;
+//		foreach (Participant participant in PlayGamesPlatform.Instance.RealTime.GetConnectedParticipants())
+//		{
+//			if (participant == PlayGamesPlatform.Instance.RealTime.GetSelf() && number == 0)
+//			{
+//				return true;
+//			}
+//			else
+//			{
+//				return false;
+//			}
+//			number++;
+//		}
+//		return false;
+//#endif
+//	}
 	
 }
