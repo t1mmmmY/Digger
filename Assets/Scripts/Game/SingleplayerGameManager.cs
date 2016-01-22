a2f73f4c2dd936da1ab888414bcb71a12ba10499
denis <dovganichd@digicode.net>
2016-01-22 19:12:19 +0100

    Sharing is working!

--- Assets/Scripts/Game/SingleplayerGameManager.cs
+++ Assets/Scripts/Game/SingleplayerGameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using VoxelBusters.NativePlugins;
+using System.IO;
 
 public class SingleplayerGameManager : GameManager 
 {
@@ -96,6 +97,11 @@ public class SingleplayerGameManager : GameManager
 	{
 		if (canShare)
 		{
+#if UNITY_ANDROID
+			StartCoroutine("ShareScreenshot");
+
+#elif UNITY_IOS
+			canShare = false;
 			// Create share sheet
 			SocialShareSheet _shareSheet 	= new SocialShareSheet();	
 			_shareSheet.Text				= "My result";
@@ -113,8 +119,60 @@ public class SingleplayerGameManager : GameManager
 			NPBinding.UI.SetPopoverPointAtLastTouchPosition(); // To show popover at last touch point on iOS. On Android, its ignored.
 			NPBinding.Sharing.ShowView(_shareSheet, OnFinishSharing);
 
+#endif
 			base.ShareGame();
 		}
+	}
+
+	public IEnumerator ShareScreenshot()
+	{
+		canShare = false;
+
+		// wait for graphics to render
+		yield return new WaitForEndOfFrame();
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+		// create the texture
+		Texture2D screenTexture = new Texture2D(Screen.width, Screen.height,TextureFormat.RGB24,true);
+
+		// put buffer into texture
+		screenTexture.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height),0,0);
+
+		// apply
+		screenTexture.Apply();
+		//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- PHOTO
+
+		byte[] dataToSave = screenTexture.EncodeToPNG();
+
+		string destination = Path.Combine(Application.persistentDataPath,System.DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + ".png");
+
+		File.WriteAllBytes(destination, dataToSave);
+
+		if(!Application.isEditor)
+		{
+			// block to open the file and share it ------------START
+			AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+			AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+			intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_SEND"));
+			AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+			AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse","file://" + destination);
+			intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject);
+			//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_TEXT"), "testo");
+			//intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_SUBJECT"), "SUBJECT");
+			intentObject.Call<AndroidJavaObject>("setType", "image/jpeg");
+			AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+			AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+
+			// option one WITHOUT chooser:
+			currentActivity.Call("startActivity", intentObject);
+
+			// option two WITH chooser:
+			//AndroidJavaObject jChooser = intentClass.CallStatic<AndroidJavaObject>("createChooser", intentObject, "YO BRO! WANNA SHARE?");
+			//currentActivity.Call("startActivity", jChooser);
+
+			// block to open the file and share it ------------END
+
+		}
+		canShare = true;
 	}
 
 	void OnFinishSharing(eShareResult _result)
